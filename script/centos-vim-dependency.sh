6ed893c604540eb22dd9c69e9248ca8c9d52148d
tinoji <pot5.sunen.on.the.earth.614@gmail.com>
2018-05-02 20:27:59 +0900

    Add vim dependency script for centos

---
+++ script/centos-vim-dependency.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+yum install -y https://centos6.iuscommunity.org/ius-release.rpm &&\
+yum install -y python36 &&\
+ln -s /usr/bin/python3.6 /usr/bin/python3 &&\
+yum -y install python-pip &&\
+python3 -m ensurepip &&\
+pip2 install neovim &&\
+pip3 install neovim
+
