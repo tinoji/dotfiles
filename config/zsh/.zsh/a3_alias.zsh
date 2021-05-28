fe819ad1b6bd76bb28cec84b8e5d8410ee7bb411
tinoji <pot5.sunen.on.the.earth.614@gmail.com>
2021-05-28 13:06:06 +0900

    Add: alias for notify to desktop

--- config/zsh/.zsh/a3_alias.zsh
+++ config/zsh/.zsh/a3_alias.zsh
@@ -2,7 +2,11 @@ alias u='cd ..'
 alias ls='ls -G'
 alias ll='ls -laGh'
 alias grep='grep --color'
-alias sed=gsed
+alias sed='gsed'
 alias df='df -h'
-alias rm=rmtrash
+alias rm='rmtrash'
+alias k='kubectl'
 
+if [ $commands[notify] ]; then
+    alias notifycmd='notify --timeout false -m "done: $?"'
+fi
